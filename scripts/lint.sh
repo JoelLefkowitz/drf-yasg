aeb7877fc1bdbb7872c51327201494e51cf1e549
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-10-11 12:31:38 +0100

    Move linting and formatting conf into their scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -ev
 
-flake8 src/drf_yasg2 testproj tests setup.py
+TARGETS='src/drf_yasg2 testproj tests setup.py'
+IGNORE='E230,E231,E501,F405,W504'
+EXCLUDE='**/migrations/*'
+
+echo $TARGETS --ignore $IGNORE --exclude $EXCLUDE --count
+flake8 $TARGETS --ignore $IGNORE --exclude $EXCLUDE --count
