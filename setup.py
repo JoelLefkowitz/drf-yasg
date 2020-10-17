454a79a8cad18bd3fffa6092561c4f21b7fe450b
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-10-17 16:06:14 +0100

    Remove duplicate dependecy

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
             "uritemplate>=3.0.1",
         ],
         extras_require={
-            "validation": ["swagger-spec-validator>=2.1.0"],
             "lint": ["flake8>=3.5.0"],
             "format": ["autoflake>=1.4.0", "black>=20.8.0", "isort>=4.2.0"],
             "dist": ["bump2version", "wheel"],
